9e92008d54020fcccc07806c3f9b8449d472ee86
Matt DeKrey <matt.dekrey@responsivepath.com>
2015-01-28 09:36:17 -0500

    Add a basic ILogConfiguration implementation. Enable XML comments for the Release mode.

---
+++ ResponsivePath.Logging/Logging/LogConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsivePath.Logging
+{
+    /// <summary>
+    /// Basic ILogConfiguration implementation
+    /// </summary>
+    public class LogConfiguration : ILogConfiguration
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public LogConfiguration()
+        {
+            Accumulators = new List<IDataAccumulator>();
+            Indexers = new List<ILogIndexer>();
+            Recorders = new List<ILogRecorder>();
+        }
+
+        IEnumerable<IDataAccumulator> ILogConfiguration.Accumulators
+        {
+            get { return Accumulators; }
+        }
+
+        IEnumerable<ILogIndexer> ILogConfiguration.Indexers
+        {
+            get { return Indexers; }
+        }
+
+        IEnumerable<ILogRecorder> ILogConfiguration.Recorders
+        {
+            get { return Recorders; }
+        }
+
+        /// <summary>
+        /// A list of data accumulators to use with the logger
+        /// </summary>
+        public IList<IDataAccumulator> Accumulators { get; private set; }
+
+        /// <summary>
+        /// A list of indexers to use with the logger
+        /// </summary>
+        public IList<ILogIndexer> Indexers { get; private set; }
+
+        /// <summary>
+        /// A list of recorders for each log entry
+        /// </summary>
+        public IList<ILogRecorder> Recorders { get; private set; }
+    }
+}
